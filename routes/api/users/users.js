cda4f6bfeaa90c082969e70b00ea2315514232a4
Andrii Vakhotskyi <andre2603foreigner@gmail.com>
2021-03-31 04:54:52 +0300

    hw-06

--- routes/api/users/users.js
+++ routes/api/users/users.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../../../controllers/controllerUsers");
 const guard = require("../../../helpers/guard");
-const {accountLimiter} = require("../../../helpers/rateLimits");
+// const {accountLimiter} = require("../../../helpers/rateLimits");
 const upload = require("../../../helpers/upload");
 const {validateUploadAvatar} = require("./validation");
 
-router.post("/register", accountLimiter, usersController.reg);
+// accountLimiter, - message - not found
+
+router.post("/register", usersController.reg);
 router.post("/login", usersController.login);
 router.post("/logout", guard, usersController.logout);
 router.get("/current", guard, usersController.getCurrentUser);
@@ -15,5 +17,6 @@ router.patch(
   [guard, upload.single("avatar"), validateUploadAvatar],
   usersController.avatars
 );
+router.get("/verify/:verifyToken", usersController.verify);
 
 module.exports = router;
